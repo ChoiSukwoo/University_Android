8e22996a48c54e5d4b15f4239005e7582219e6ad
ChoiSukwoo <58356541+ChoiSukwoo@users.noreply.github.com>
2020-11-17 16:19:58 +0900

    12주차 Tabbar 프로젝트
    12주차 Tabbar 프로젝트
    tabbar인데 bottombar로 프로젝트 네이밍 실수

--- WEEK12/BottomBar/app/src/main/java/com/example/bottombar/MainActivity.java
+++ WEEK12/BottomBar/app/src/main/java/com/example/bottombar/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.bottombar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    Fragment1 frame1;
+    Fragment2 frame2;
+    Fragment3 frame3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        frame1 = new Fragment1();
+        frame2 = new Fragment2();
+        frame3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,frame1).commit();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("통화기록"));
+        tabs.addTab(tabs.newTab().setText("스팸기록"));
+        tabs.addTab(tabs.newTab().setText("연락처"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("탭 선택","선택된 탭 : " + position);
+                Fragment Selected  = null;
+                if(position == 0){
+                    Selected = frame1;
+                }else if(position == 1){
+                    Selected = frame2;
+                }else if(position == 2){
+                    Selected = frame3;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,Selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
